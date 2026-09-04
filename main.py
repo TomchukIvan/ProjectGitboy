c76488c32c0808c1a948b4525c7318933b1a6c3b
TomchukIvan <vanchester1283@gmail.com>
2026-09-04 15:47:27 +0300

    Добавил вывод текущей даты

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from datetime import date
+from datetime import date, datetime
 
 Data1 = date(2020, 12, 31)
 Data2 = date(2001, 2, 16)
 print(Data1)
 print(Data2)
+
+DataNow = datetime.now()
+print(DataNow)
